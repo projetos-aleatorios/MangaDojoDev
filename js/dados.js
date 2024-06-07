5c4c6766a5f9a32d7ec8c14de191eb754be276ab
MatVzn <matteovoleite@gmail.com>
2024-06-06 22:16:32 -0300

    painel de dados inicial

---
+++ js/dados.js
@@ -0,0 +1,71 @@
+var TWITTER = ""
+var DISCORD = ""
+
+var MANGAS = [
+  
+  // LEMBRE-SE sempre de escrever os conteúdos entre "aspas";
+  // CUIDADO para não apagar conteúdos por acidente, vírgulas, pontuação, etc.
+
+
+
+  // ONE PIECE
+
+  { "name": "One Piece", "author": "Eichiiro Oda", "post-type": "Capítulo", "img": "./assets/mangas/CoverOnePiece.png",
+    "all-links": "https://cubari.moe/read/gist/cmF3L2p2ZHVkYWJvbm9uaS90cjNxeHovbWFpbi9PbmVQaWVjZS5qc29u/",
+
+    "last-content": "1116",
+    "last-title": "Conflito",
+    "next-date": "??/??/????"
+  },
+
+  // JUJUTSU KAISEN
+
+  { "name": "Jujutsu Kaisen", "author": "Gege Akutami", "post-type": "Capítulo", "img": "./assets/mangas/CoverJujutsu.png",
+    "all-links": "https://cubari.moe/read/gist/cmF3L2p2ZHVkYWJvbm9uaS90cjNxeHovbWFpbi9qdWp1dHN1a2FpbnNlLmpzb24/",
+
+    "last-content": "262",
+    "last-title": "A Batalha na Terra Assombrada de Shinjuku",
+    "next-date": "??/??/????" 
+  },
+
+  // SAKAMOTO DAYS
+
+  { "name": "Sakamoto Days", "author": "Yuto Suzuki", "post-type": "Capítulo", "img": "./assets/mangas/CoverSakamoto.png",
+    "all-links": "https://cubari.moe/read/gist/cmF3L2p2ZHVkYWJvbm9uaS90cjNxeHovbWFpbi9zYWthbW90b2RheXMuanNvbg",
+
+    "last-content": "168",
+    "last-title": "Museu Fechado",
+    "next-date": "??/??/????" 
+  },
+
+  // USOGUI
+
+  { "name": "Usogui", "author": "Toshio Sako", "post-type": "Capítulo", "img": "./assets/mangas/CoverUsogui.png",
+    "all-links": "https://mangadex.org/group/a03ed985-e384-4b54-800a-09a25130a7ae/manga-dojo?tab=feed",
+
+    "last-content": "4",
+    "last-title": "Assuntos Secretos",
+    "next-date": "??/??/????" 
+  },
+
+  // KONO SEKAI WA FUKANZEN SUGIRU
+
+  { "name": "Kono Sekai wa Fukanzen Sugiru", "author": "Masamichi Sato", "post-type": "Capítulo", "img": "./assets/mangas/CoverKonoSekai.png",
+    "all-links": "https://mangadex.org/group/a03ed985-e384-4b54-800a-09a25130a7ae/manga-dojo?tab=feed",
+
+    "last-content": "2",
+    "last-title": "Makoto Haga",
+    "next-date": "??/??/????" 
+  },
+
+  // THE RAVAGES OF TIME
+
+  { "name": "The Ravages of Time", "author": "Chen Mou", "post-type": "Volume", "img": "./assets/mangas/CoverTheRavages.png",
+    "all-links": "",
+
+    "last-content": "",
+    "last-title": "",
+    "next-date": ""
+  },
+
+]
